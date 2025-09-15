d413c9a61951c615d915636e7fce5929cba13491
JulianaHansen <juliana.hansen@engenharia.ufjf.br>
2025-09-15 17:02:05 -0300

    comentario de legenda

--- contracts/poap_badge/src/storage.rs
+++ contracts/poap_badge/src/storage.rs
@@ -3,25 +3,30 @@ use soroban_sdk::{Env, Symbol, Address, BytesN, Vec};
 pub fn get_user_badges(env: &Env, user: &Address) -> Vec<BytesN<32>> {
     env.storage()
         .persistent()
-        .get(&(Symbol::new(env, "user_badges"), user.clone()))
+        .get(&(Symbol::new(env, "ub"), user.clone()))
         .unwrap_or(Vec::new(env))
 }
 
 pub fn set_user_badges(env: &Env, user: &Address, badges: &Vec<BytesN<32>>) {
     env.storage()
         .persistent()
-        .set(&(Symbol::new(env, "user_badges"), user.clone()), badges);
+        .set(&(Symbol::new(env, "ub"), user.clone()), badges);
 }
 
 pub fn get_event_owners(env: &Env, event_id: &BytesN<32>) -> Vec<Address> {
     env.storage()
         .persistent()
-        .get(&(Symbol::new(env, "event_owners"), event_id.clone()))
+        .get(&(Symbol::new(env, "eo"), event_id.clone()))
         .unwrap_or(Vec::new(env))
 }
 
 pub fn set_event_owners(env: &Env, event_id: &BytesN<32>, owners: &Vec<Address>) {
     env.storage()
         .persistent()
-        .set(&(Symbol::new(env, "event_owners"), event_id.clone()), owners);
+        .set(&(Symbol::new(env, "eo"), event_id.clone()), owners);
 }
+
+// eo = event_owners
+// ub = user_badges
+// em = event_metadata
+// ev = events_list
