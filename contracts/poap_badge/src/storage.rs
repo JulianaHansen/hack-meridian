38cf2cbda868d6dd31bd034623cac3f8639930ff
JulianaHansen <juliana.hansen@engenharia.ufjf.br>
2025-09-15 21:32:25 -0300

    funcao de listar todas as badges

--- contracts/poap_badge/src/storage.rs
+++ contracts/poap_badge/src/storage.rs
@@ -1,4 +1,13 @@
-use soroban_sdk::{Env, Symbol, Address, BytesN, Vec};
+use soroban_sdk::{Env, Symbol, Address, BytesN, Vec, contracttype};
+
+use crate::event::{list_events, get_event_metadata, EventMetadata};
+
+#[derive(Clone)]
+#[contracttype] // necessário para serializar no Soroban
+pub struct BadgeInfo {
+    pub event_id: BytesN<32>,
+    pub metadata: EventMetadata,
+}
 
 // Pega as badges (eventos) de um usuário
 pub fn get_user_badges(env: &Env, user: &Address) -> Vec<BytesN<32>> {
@@ -6,6 +15,12 @@ pub fn get_user_badges(env: &Env, user: &Address) -> Vec<BytesN<32>> {
         .persistent()
         .get(&(Symbol::new(env, "ub"), user.clone()))
         .unwrap_or(Vec::new(env))
+}
+
+pub fn set_user_badges(env: &Env, user: &Address, badges: &Vec<BytesN<32>>) {
+    env.storage()
+        .persistent()
+        .set(&(Symbol::new(env, "ub"), user.clone()), badges);
 }
 
 // Adiciona uma badge (evento) a um usuário
@@ -17,12 +32,23 @@ pub fn add_user_badge(env: &Env, user: &Address, badge: &BytesN<32>) {
     }
 }
 
-// Lista todas as badges (eventos) criadas
-pub fn list_all_badges(env: &Env) -> Vec<BytesN<32>> {
-    env.storage()
-        .persistent()
-        .get(&Symbol::new(env, "ev"))
-        .unwrap_or(Vec::new(env))
+// Lista todas as badges criadas na galeria
+pub fn list_all_badges(env: &Env) -> Vec<BadgeInfo>{
+    let mut badges = Vec::new(env);
+
+    let events = list_events(env.clone());
+
+    for event_id in events.iter() {
+        let metadata = get_event_metadata(env.clone(), event_id.clone());
+        let badge_info = BadgeInfo {
+            event_id: event_id.clone(),
+            metadata,
+        };
+        badges.push_back(badge_info);
+    }
+
+    badges
+
 }
 
 // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
